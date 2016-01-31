3ad42b768b573740e71bcfde45827636fa9e1a63
lichunqiang <light-li@hotmail.com>
2016-01-31 20:43:48 +0800

    remove unused css

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
         },
         files: {
           // compilation.css  :  source.less
-          "dist/css/AdminLTE.css": "build/less/AdminLTE.less",
+          "dist/css/AdminLTE.css": "build/less/AdminLTE_v1.less",
           //Non minified skin files
           "dist/css/skins/skin-blue.css": "build/less/skins/skin-blue.less",
           "dist/css/skins/skin-black.css": "build/less/skins/skin-black.less",
